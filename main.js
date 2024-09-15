ad9d8e64fc19806d7c87b64a25d7f6938a00c997
theomorphic <FelixMK@yandex.ru>
2024-09-15 21:09:55 +0500

    feat: finish ux

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ previewImg = uploadBox.querySelector("img"),
 fileInput = uploadBox.querySelector("input"),
 widthInput = document.querySelector(".width input"),
 heightInput = document.querySelector(".height input"),
-ratioInput = document.querySelector(".ratio input");
+ratioInput = document.querySelector(".ratio input"),
+qualityInput = document.querySelector(".quality input"),
+downloadBtn = document.querySelector(".download-btn");
 
 let ogImageRatio;
 
@@ -31,5 +33,23 @@ heightInput.addEventListener("keyup", ()=>{
 	 widthInput.value = Math.floor(width);
 });
 
+const resizeAndDownload = ()=>{
+	const canvas = document.createElement("canvas");
+	const a = document.createElement("a");
+	const ctx = canvas.getContext("2d");
+
+	const imgQuality = qualityInput.checked ? 0.7 : 1.0;
+
+	canvas.width = widthInput.value;
+	canvas.height= heightInput.value;
+
+	ctx.drawImage(previewImg, 0, 0, canvas.width, canvas.height);
+
+	a.href = canvas.toDataURL("./img/jpeg", imgQuality);
+	a.download = new Date().getTime();
+	a.click();
+}
+
+downloadBtn.addEventListener("click", resizeAndDownload);
 fileInput.addEventListener("change", loadFile);
 uploadBox.addEventListener("click", () => fileInput.click());
